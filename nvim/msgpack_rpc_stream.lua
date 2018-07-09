c9763beaea2d0cab179d2831a6902f2ee56fa264
Peter Hodge <peter.hodge84@gmail.com>
2018-07-09 16:17:41 +1000

    msgpack_rpc_stream: copy of hexdump() helper from neovim project

--- nvim/msgpack_rpc_stream.lua
+++ nvim/msgpack_rpc_stream.lua
@@ -11,6 +11,31 @@ local Tabpage = {}
 Tabpage.__index = Tabpage
 function Tabpage.new(id) return setmetatable({id=id}, Tabpage) end
 
+local function hexdump(str)
+  local len = string.len(str)
+  local dump = ""
+  local hex = ""
+  local asc = ""
+
+  for i = 1, len do
+    if 1 == i % 8 then
+      dump = dump .. hex .. asc .. "\n"
+      hex = string.format("%04x: ", i - 1)
+      asc = ""
+    end
+
+    local ord = string.byte(str, i)
+    hex = hex .. string.format("%02x ", ord)
+    if ord >= 32 and ord <= 126 then
+      asc = asc .. string.char(ord)
+    else
+      asc = asc .. "."
+    end
+  end
+
+  return dump .. hex .. string.rep("   ", 8 - len % 8) .. asc
+end
+
 local Response = {}
 Response.__index = Response
 
